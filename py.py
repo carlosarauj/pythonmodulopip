fa82ee0bac228b1f1c6aba85842d8aa861821aa2
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-05-09 21:22:37 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,7 @@
+import tools
+
+print(tools.grav)
+
+print(tools.highest_number([2342,656585,8755,5,36,3,7,478,]))
+
+#usa o terminal com: python nomedoarquivo.py
---
+++ tools.py
@@ -0,0 +1,8 @@
+num1 = 2
+grav = 9.8
+
+def get_extension(file):
+    return file[file.index(".") +1:]
+
+def highest_number(numbers):
+    return max(numbers)
